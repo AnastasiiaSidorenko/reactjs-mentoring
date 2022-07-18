f8b99e3249209b6c27ea7d0c40e142a0d552050c
Anastasiia Sidorenko <anastasiia_sidorenko@epam.com>
2022-07-19 02:18:58 +0300

    wip

--- movies-app/src/Header/Header.jsx
+++ movies-app/src/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { I18N } from '../core/i18n';
 import { LocaleContext } from '../core/i18n/locale';
 import './Header.scss';
@@ -7,24 +7,27 @@ import PlusIcon from '../core/constants/plus-icon.svg';
 
 export const Header = () => {
   const locale = useContext(LocaleContext);
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo" src={logo} alt="logo" />
-        <button className="add-button" type="button">
-          <PlusIcon />
-          {I18N[locale].ADD_MOVIE}
-        </button>
-      </div>
-      <div className="search-movie">
-        <p className="search-movie__title">{I18N[locale].FIND_YOUR_MOVIE}</p>
-        <div className="search-movie__field">
-          <input className="input" value="" placeholder="What do you want to watch?" />
-          <button className="button" type="button">
-            {I18N[locale].SEARCH}
+    <>
+      <div className="header">
+        <div className="logo-container">
+          <img className="logo" src={logo} alt="logo" />
+          <button className="add-button" type="button" onClick={() => setIsOpen(true)}>
+            <PlusIcon />
+            {I18N[locale].ADD_MOVIE}
           </button>
         </div>
+        <div className="search-movie">
+          <p className="search-movie__title">{I18N[locale].FIND_YOUR_MOVIE}</p>
+          <div className="search-movie__field">
+            <input className="input" value="" placeholder="What do you want to watch?" />
+            <button className="button" type="button">
+              {I18N[locale].SEARCH}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
